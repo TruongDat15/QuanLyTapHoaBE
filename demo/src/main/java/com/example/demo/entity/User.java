c2dbea2afd38e745c1802acfd5c9104cf67ce012
TruongDat15 <100866308+TruongDat15@users.noreply.github.com>
2025-10-09 20:36:31 +0700

    testCommit

---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data // Tự động tạo Getters, Setters, requiredArgsConstructor, toString, equals và hashCode
+@NoArgsConstructor // Tự động tạo constructor không tham số
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
